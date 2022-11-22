02d8213a35e5cb7efab5efb50e3c7b8673cd645e
FadirTorcer <eric.lyonford@gmail.com>
2022-11-21 22:16:20 -0600

    Wrapped up Part 4??

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -70,9 +70,12 @@ public class HomeController {
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-
+        Optional<Job> optJob = jobRepository.findById(jobId);
+        if (optJob.isPresent()) {
+            Job job = (Job) optJob.get();
+            model.addAttribute("job", job);
+        }
         return "view";
     }
-
 
 }
